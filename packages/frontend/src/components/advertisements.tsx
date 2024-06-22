409946205dbf1c2ce65174b8813cae33880924b4
Fraser Rennie <fjrennie1@outlook.com>
2024-06-22 21:37:18 +0100

    website

--- packages/frontend/src/components/advertisements.tsx
+++ packages/frontend/src/components/advertisements.tsx
@@ -41,18 +41,18 @@ export function GordonsAd() {
                     Deal of the summer alert
                   </DialogTitle>
                   <div>
-                    <h1 className="text-5xl uppercase text-white">Buy Gordon's London Dry Gin</h1>
+                    <h1 className="text-5xl uppercase text-white">Buy Gordon&lsquo;s London Dry Gin</h1>
                     <p className="mt-8 text-justify">
                       Now on sale for just £12.99 a bottle. Speak to your local dealer for more
                       information. Terms and conditions apply. You must be 18+ and live in an
                       eligible region. The fun runs from [Start Date] to [End Date], so get your gin
                       on! Winners will be announced within 14 days of the promo end and must claim
-                      their prize within 30 days or we’ll assume you got lost in a gin haze. By
+                      their prize within 30 days or we&lsquo;ll assume you got lost in a gin haze. By
                       entering, you let us use your name and face in our marketing—yay for fame!
-                      Sneaky part: you’re auto-enrolled in our exclusive membership at $19.99/month
-                      (surprise!). We’ll charge your card monthly, but hey, cool club perks! Extra
+                      Sneaky part: you&lsquo;re auto-enrolled in our exclusive membership at $19.99/month
+                      (surprise!). We&lsquo;ll charge your card monthly, but hey, cool club perks! Extra
                       costs like taxes and delivery are on you, so read the fine print. We can
-                      change the rules whenever because we’re the gin bosses. Disputes go to
+                      change the rules whenever because we&lsquo;re the gin bosses. Disputes go to
                       [Jurisdiction]. Questions? Contact our awesome customer service at [Contact
                       Information]. Cheers and good luck!
                     </p>
