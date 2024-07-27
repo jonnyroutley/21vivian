d51bfd9aee70723b0af30186d9f14c100f24450a
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2024-07-27 13:29:13 +0100

    remove localhost refs

--- packages/frontend/src/app/actions.ts
+++ packages/frontend/src/app/actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache"
 
 import { paths } from "@/client/schema"
 import { NewReview, createReview } from "@/hooks/reviews"
+import { config } from "@/lib/config"
 
 export type Reviews =
   paths["/reviews"]["get"]["responses"]["201"]["content"]["application/json; charset=utf-8"]
@@ -19,7 +20,7 @@ export async function createReviewAction(newReview: NewReview) {
 }
 
 export async function getReviews() {
-  const query = await fetch("http://localhost:8000/reviews", { cache: "no-store" })
+  const query = await fetch(`${config.apiBaseUrl}/reviews`, { cache: "no-store" })
 
   const json = await query.json()
   const okay = json as Reviews
--- packages/frontend/src/hooks/reviews.ts
+++ packages/frontend/src/hooks/reviews.ts
@@ -1,20 +1,21 @@
+import { config } from "@/lib/config"
+
 import { components, paths } from "../client/schema"
 
 export type Review = components["schemas"]["Review"]
 export type NewReview = components["schemas"]["InputModel"]
-
 
 type CreateReview =
   paths["/reviews"]["post"]["requestBody"]["content"]["application/json; charset=utf-8"]
 
 export async function createReview(data: CreateReview) {
   console.log("posting", data)
-  const data2 =  await fetch("http://localhost:8000/reviews", {
+  const data2 = await fetch(`${config.apiBaseUrl}/reviews`, {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
   console.debug(data2)
 
