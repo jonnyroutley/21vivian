c74378968a4698922fbfe07824217f4f4cebdb1d
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2024-11-04 23:18:06 +0000

    GOTM

---
+++ packages/frontend/src/app/gotm/FlipCard.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { url } from "inspector"
+import Image from "next/image"
+import React, { useState } from "react"
+
+const styles = `
+  /* Custom styles for 3D transforms */
+  .perspective-1000 {
+    perspective: 1000px;
+  }
+  
+  .transform-style-preserve-3d {
+    transform-style: preserve-3d;
+  }
+  
+  .backface-hidden {
+    backface-visibility: hidden;
+  }
+  
+  .rotate-y-180 {
+    transform: rotateY(180deg);
+  }
+`
+
+const girlfriends = {
+  girlfriend1: {
+    name: "July 24",
+    boyfriend: "Fraser",
+    girlfriend: "Gigi",
+    url: "/girlfriends/gigi.webp",
+  },
+  girlfriend2: {
+    name: "August 24",
+    boyfriend: "Jonny",
+    girlfriend: "Anna",
+    url: "/girlfriends/anna.jpg",
+  },
+  girlfriend3: {
+    name: "September 24",
+    boyfriend: "Luke",
+    girlfriend: "Jess",
+    url: "/girlfriends/jess.jpg",
+  },
+  girlfriend4: {
+    name: "October 24",
+    girlfriend: "Lucy",
+    boyfriend: "George",
+    url: "/girlfriends/lucy.jpg",
+  },
+} as const
+
+export const FlipCard = ({ girlfriendKey }: { girlfriendKey: keyof typeof girlfriends }) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  console.log(girlfriends, girlfriendKey)
+  const { name, boyfriend, girlfriend, url } = girlfriends[girlfriendKey]
+
+  return (
+    <>
+      <style>{styles}</style>
+      <div className="perspective-1000 h-96 w-64">
+        <div
+          className={`transform-style-preserve-3d relative h-full w-full cursor-pointer transition-transform duration-500 ${
+            isFlipped ? "rotate-y-180" : ""
+          }`}
+          onClick={() => setIsFlipped(!isFlipped)}
+        >
+          {/* Front of card */}
+          <div className="backface-hidden absolute flex h-full w-full flex-col items-center justify-center rounded-sm border-4 border-amber-900 bg-neutral-800 p-6 shadow-lg">
+            <div className="relative flex h-48 w-48 items-center justify-center rounded-full bg-neutral-900">
+              <span className="absolute z-0 text-8xl font-bold text-neutral-800">?</span>
+              <h2 className="relative z-10 mb-4 text-2xl font-bold text-amber-300">{name}</h2>
+            </div>
+          </div>
+
+          {/* Back of card */}
+          <div className="backface-hidden rotate-y-180 absolute flex h-full w-full flex-col items-center justify-center gap-4 rounded-sm border-4 border-amber-900 bg-neutral-800 p-6 shadow-lg">
+            {/* <div className="flex h-32 w-32 items-center rounded-full"> */}
+            <h2 className="text-center text-2xl font-bold text-amber-300">✨{girlfriend}✨</h2>
+            <Image
+              alt="gf"
+              src={url}
+              width={300}
+              height={300}
+              className="h-48 w-48 rounded-full object-cover"
+            />
+            {/* </div> */}
+            <h2 className="mb-4 text-center text-xl text-amber-300">Girlfriend of: {boyfriend}</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ packages/frontend/src/app/gotm/page.tsx
@@ -0,0 +1,32 @@
+import { Blob1, Blob2, Blob3 } from "@/components/blobs"
+
+import { FlipCard } from "./FlipCard"
+
+export default function GirlfriendOfTheMonth() {
+  return (
+    <div className="relative h-screen w-full overflow-hidden bg-[#626791]">
+      <Blob1 fill="#B15E81" className="absolute left-[5%] top-[30%] h-64" />
+      <Blob2 fill="#59838D" className="absolute left-[10%] top-[50%] h-96" />
+      <Blob3 fill="#D18D6A" className="absolute left-[15%] top-[20%] h-32" />
+      <Blob1 fill="#B15E81" className="absolute left-[40%] top-[60%] h-96" />
+      <Blob2 fill="#59838D" className="absolute left-[35%] top-[10%] h-80" />
+      <Blob3 fill="#D18D6A" className="absolute left-[60%] top-[30%] h-48" />
+      <Blob1 fill="#B15E81" className="absolute left-[70%] top-[10%] h-64" />
+      <Blob2 fill="#59838D" className="absolute left-[80%] top-[40%] h-24" />
+      <Blob3 fill="#D18D6A" className="absolute left-[80%] top-[50%] h-32" />
+      <div className="mt-8 flex w-full flex-col justify-center gap-12 lg:mt-0">
+        <div className="mx-auto flex min-h-screen w-fit flex-col items-center justify-center gap-12 px-8">
+          <h1 className="z-10 text-7xl text-amber-300">Girlfriend of the month</h1>
+          <div className="flex flex-shrink-0 flex-col items-center gap-12 lg:flex-row">
+            {/* <div className="z-10 h-96 w-64 bg-amber-800"></div> */}
+
+            <FlipCard girlfriendKey="girlfriend4" />
+            <FlipCard girlfriendKey="girlfriend3" />
+            <FlipCard girlfriendKey="girlfriend2" />
+            <FlipCard girlfriendKey="girlfriend1" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
