b5dde1cafa2530867398bd57173c9e4639ce729e
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2025-10-04 15:45:47 +0100

    remove log

--- packages/frontend/src/app/energy/page.tsx
+++ packages/frontend/src/app/energy/page.tsx
@@ -5,7 +5,6 @@ import { EnergyChart } from "./energyChart"
 export default async function EnergyPage() {
   const gasUsage = await getGasUsage()
   const electricityUsage = await getElectricityUsage()
-  console.log(gasUsage[0], electricityUsage[0])
   return (
     <main
       className={`flex min-h-screen w-full flex-col items-center bg-neutral-950 pt-8 font-mono text-neutral-50`}
