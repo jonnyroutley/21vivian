828def1a01c2c47cf2923ff21e7f71516b39a642
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2025-10-04 15:49:32 +0100

    try fix

--- packages/frontend/src/app/energy/page.tsx
+++ packages/frontend/src/app/energy/page.tsx
@@ -1,16 +1,19 @@
-import { getElectricityUsage, getGasUsage } from "@/actions/energy"
+import { getElectricityUsage, getGasUsage } from "@/actions/energy";
 
-import { EnergyChart } from "./energyChart"
+import { EnergyChart } from "./energyChart";
 
 export default async function EnergyPage() {
-  const gasUsage = await getGasUsage()
-  const electricityUsage = await getElectricityUsage()
-  return (
-    <main
-      className={`flex min-h-screen w-full flex-col items-center bg-neutral-950 pt-8 font-mono text-neutral-50`}
-    >
-      {/* <h1 className="my-4 text-4xl text-yellow-400">Energy usage</h1> */}
-      <EnergyChart gasData={gasUsage} electricityData={electricityUsage} />
-    </main>
-  )
+	const gasUsage = await getGasUsage();
+	const electricityUsage = await getElectricityUsage();
+	if (!gasUsage || !electricityUsage) {
+		return null;
+	}
+	return (
+		<main
+			className={`flex min-h-screen w-full flex-col items-center bg-neutral-950 pt-8 font-mono text-neutral-50`}
+		>
+			{/* <h1 className="my-4 text-4xl text-yellow-400">Energy usage</h1> */}
+			<EnergyChart gasData={gasUsage} electricityData={electricityUsage} />
+		</main>
+	);
 }
